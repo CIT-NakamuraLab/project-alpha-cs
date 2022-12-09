4f5161fea55e1d7f9e3a5cab54d9165a147328c3
AInine <ainine.jp@gmail.com>
2022-12-09 14:54:37 +0900

    リーダー登録用のtRPCを追加

---
+++ src/server/trpc/router/reader.ts
@@ -0,0 +1,22 @@
+import { protectedProcedure, router } from "../trpc";
+import { z } from "zod";
+import crypto from "crypto";
+
+const N = 16
+const clientId = crypto.randomBytes(N).toString('base64').substring(0, N)
+const clientToken = crypto.randomBytes(N).toString('base64').substring(0, N)
+const clientTokenHash = crypto.createHash('sha256').update(clientToken).digest('hex')
+export const readerRouter = router ({
+  register: protectedProcedure
+    .input(z.object({name: z.string()}))
+    .query(async ({ctx, input}) => {
+      const object = {data: {
+          name: input.name,
+          client_id: clientId,
+          client_token_hash: clientTokenHash
+        }
+      }
+      const result = await ctx.prisma.reader.create(object)
+      return {client_token: clientToken, ...result}
+    })
+})
