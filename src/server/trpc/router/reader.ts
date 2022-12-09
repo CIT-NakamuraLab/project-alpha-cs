2dccd697dc7db2989e8e1bc3b1056c42b8d2bf32
AInine <ainine.jp@gmail.com>
2022-12-09 16:14:45 +0900

    リーダーのIDとトークンの生成をutilに移動

--- src/server/trpc/router/reader.ts
+++ src/server/trpc/router/reader.ts
@@ -1,19 +1,16 @@
 import { protectedProcedure, router } from "../trpc";
 import { z } from "zod";
-import crypto from "crypto";
+import { generateClientId, generateClientToken, generateHashedToken } from "../../../utils/client";
 
-const N = 16
-const clientId = crypto.randomBytes(N).toString('base64').substring(0, N)
-const clientToken = crypto.randomBytes(N).toString('base64').substring(0, N)
-const clientTokenHash = crypto.createHash('sha256').update(clientToken).digest('hex')
 export const readerRouter = router ({
   register: protectedProcedure
     .input(z.object({name: z.string()}))
     .query(async ({ctx, input}) => {
+      const clientToken = generateClientToken()
       const object = {data: {
           name: input.name,
-          client_id: clientId,
-          client_token_hash: clientTokenHash
+          client_id: generateClientId(),
+          client_token_hash: generateHashedToken(clientToken)
         }
       }
       const result = await ctx.prisma.reader.create(object)
