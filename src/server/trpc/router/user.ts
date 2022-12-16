e490d437406658471737c39e6104f3e3b3d6edb5
AInine <ainine.jp@gmail.com>
2022-12-16 14:04:39 +0900

    ユーザー用のtRPCを追加

---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,26 @@
+import { protectedProcedure, router } from "../trpc";
+import { z } from "zod";
+
+export const userRouter = router({
+  setStudentId: protectedProcedure
+    .input(z.object({ id: z.string(), studentId: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return await ctx.prisma.user.update({
+        where: {
+          id: input.id
+        },
+        data: {
+          student_id: input.studentId
+        }
+      })
+    }),
+  deleteStudent: protectedProcedure
+    .input(z.object({id: z.string()}))
+    .query(async ({ctx, input}) => {
+      return await ctx.prisma.user.delete({
+        where: {
+          id: input.id
+        }
+      })
+    })
+})
