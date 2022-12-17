9b1fbbc19ef322f2699ef6e424115b7c2612bbb7
quadseed <contact@quadseed.net>
2022-12-18 01:03:39 +0900

    Edit: readerをuserをrouterに追加

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,12 @@
 import { router } from '../trpc'
 import { authRouter } from './auth'
+import { readerRouter } from './reader'
+import { userRouter } from './user'
 
 export const appRouter = router({
-  auth: authRouter
+  auth: authRouter,
+  reader: readerRouter,
+  user: userRouter
 })
 
 // export type definition of API
