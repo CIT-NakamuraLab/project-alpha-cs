ad441eaf7ff62201f1a34add4bd33cb5177a5a75
quadseed <contact@quadseed.net>
2022-12-22 17:56:26 +0900

    Edit: google OAuth用のパッケージと設定を追加

--- node-env.d.ts
+++ node-env.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly SLACK_CLIENT_ID: string
     readonly SLACK_CLIENT_SECRET: string
+
+    readonly GOOGLE_CLIENT_ID: string
+    readonly GOOGLE_CLIENT_SECRET: string
+    readonly GOOGLE_CALLBACK_URL: string
   }
 }
