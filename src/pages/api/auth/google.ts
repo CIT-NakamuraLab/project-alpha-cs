93fe236b6c76cd083fc0f99c6c617e2af6e362df
quadseed <contact@quadseed.net>
2022-12-24 19:54:16 +0900

    Edit: 学生番号を切り出す処理を実装

--- src/pages/api/auth/google.ts
+++ src/pages/api/auth/google.ts
@@ -1,19 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { google } from 'googleapis'
 import { oAuth2Client } from '../../../utils/google'
+import { getServerAuthSession } from '../../../server/common/get-server-auth-session'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.query.code == null || req.query.code == '') {
     return res.status(400).send('Bad Request')
   }
 
-  const code = req.query.code
+  const session = await getServerAuthSession({ req, res })
 
-  const { tokens } = await oAuth2Client.getToken(code as string)
-  oAuth2Client.setCredentials(tokens)
+  if (session) {
+    const code = req.query.code
 
-  const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client })
-  const response = await oauth2.userinfo.get()
+    const { tokens } = await oAuth2Client.getToken(code as string)
+    oAuth2Client.setCredentials(tokens)
 
-  res.status(200).json(response.data)
+    const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client })
+    const response = await oauth2.userinfo.get()
+
+    const user = response.data
+
+    if (!user.email?.endsWith('@s.chibakoudai.jp')) {
+      return res.status(403).json({ status: 'Error', message: 'Unsupported Account Type' })
+    }
+
+    const studentId = user.email.substring(1, 8)
+
+    res.status(200).json({ status: 'OK', student_id: studentId })
+  } else {
+    res.status(401).json({ status: 'Unauthorized', message: ' Need to Login with Slack Account' })
+  }
 }
