e8e66d09475e0dc18fd5fedc9230ee3cc369b653
AInine <ainine.jp@gmail.com>
2023-01-27 17:59:09 +0900

    リーダーから受け取るログ追加用RESTAPIエンドポイントを追加

--- sslServer.js
+++ sslServer.js
@@ -2,9 +2,12 @@ const { createServer } = require('https')
 const { parse } = require('url')
 const next = require('next')
 const fs = require('fs')
+const logController = require("./src/server/rest/logController");
+const express = require("express");
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
+const expressApp = express()
 const handle = app.getRequestHandler()
 
 const httpsOptions = {
@@ -20,4 +23,12 @@ app.prepare().then(() => {
     if (err) throw err
     console.log("> Next.js SSL supported dev Server is READY.")
   })
+
+  createServer(httpsOptions, expressApp)
+    .listen(8000, (err) => {
+      if (err) throw err
+      console.log("> Rest API Server is Ready.")
+    })
+  expressApp.use(express.json)
+  expressApp.use("/api/v1", logController)
 })
