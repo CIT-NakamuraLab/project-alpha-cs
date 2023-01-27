323d459612c1909ef779b37cbdf0af6105100bfe
AInine <ainine.jp@gmail.com>
2023-01-27 18:08:12 +0900

    フォーマッターを適用

--- src/server/rest/logController.ts
+++ src/server/rest/logController.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from "@prisma/client";
-import type {Request, Response } from "express";
-import { Router } from "express";
-import { generateHashedToken } from "../../utils/client";
+import { PrismaClient } from '@prisma/client'
+import type { Request, Response } from 'express'
+import { Router } from 'express'
+import { generateHashedToken } from '../../utils/client'
 
-const prisma = new PrismaClient();
-const router = Router();
+const prisma = new PrismaClient()
+const router = Router()
 
-router.post("/logs", async (req: Request, res: Response) => {
-  const { clientToken, studentId, type, hasKey } = req.body;
+router.post('/logs', async (req: Request, res: Response) => {
+  const { clientToken, studentId, type, hasKey } = req.body
 
   const client = await prisma.reader.findUnique({
     where: {
@@ -15,7 +15,7 @@ router.post("/logs", async (req: Request, res: Response) => {
     }
   })
   if (client == null) {
-    res.status(500).send("No client found.")
+    res.status(500).send('No client found.')
     return
   }
   const readerId = client.id
