a95a91fd96edaf792e2998166c8d8ee9f3c0cf7b
AInine <ainine.jp@gmail.com>
2023-02-24 11:05:25 +0900

    デバッグ用のconsole.logを削除

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -7,8 +7,6 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method == 'POST') {
     const { clientToken, studentId, type, hasKey } = req.body
-    console.log(clientToken, studentId, type, hasKey)
-
     const client = await prisma.reader.findUnique({
       where: {
         client_token_hash: generateHashedToken(clientToken)
