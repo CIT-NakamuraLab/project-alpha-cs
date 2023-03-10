1ca18b26660a4ed501eb46566923c82c8a1e56bc
quadseed <contact@quadseed.net>
2023-03-10 22:27:23 +0900

    ページに認証によるアクセス規制を実装

--- src/pages/settings/reader.tsx
+++ src/pages/settings/reader.tsx
@@ -1,17 +1,75 @@
+import type { GetServerSideProps } from 'next'
+import { unstable_getServerSession } from 'next-auth/next'
 import Head from 'next/head'
 import ReaderBody from '../../components/ReaderBody'
 import SettingMenu, { Select } from '../../components/SettingMenu'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { prisma } from '../../server/db/client'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-function reader() {
+function Reader(props: { isAdmin: boolean }) {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (!session) {
+        router.replace('/')
+      }
+    }
+  }, [router, session])
+
   return (
     <div>
       <Head>
         <title>カードリーダー管理</title>
       </Head>
 
-      <SettingMenu select={Select.Reader} title='カードリーダー管理' bodyElement={<ReaderBody />} />
+      <SettingMenu
+        select={Select.Reader}
+        title='カードリーダー管理'
+        isAdmin={props.isAdmin}
+        bodyElement={<ReaderBody />}
+      />
     </div>
   )
 }
 
-export default reader
+export default Reader
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  if (!session) {
+    return {
+      props: {
+        isAdmin: false
+      }
+    }
+  }
+
+  const result = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id
+    },
+    select: {
+      is_admin: true
+    }
+  })
+
+  if (!result?.is_admin) {
+    return {
+      redirect: {
+        statusCode: 302,
+        destination: '/'
+      }
+    }
+  }
+
+  return {
+    props: {
+      isAdmin: result?.is_admin
+    }
+  }
+}
--- src/pages/settings/user.tsx
+++ src/pages/settings/user.tsx
@@ -1,17 +1,75 @@
+import type { GetServerSideProps } from 'next'
+import { unstable_getServerSession } from 'next-auth/next'
+import { useSession } from 'next-auth/react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import SettingMenu, { Select } from '../../components/SettingMenu'
 import { UserBody } from '../../components/UserBody'
+import { prisma } from '../../server/db/client'
+import { authOptions } from '../api/auth/[...nextauth]'
 
-function user() {
+function User(props: { isAdmin: boolean }) {
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (!session) {
+        router.replace('/')
+      }
+    }
+  }, [router, session])
+
   return (
     <div>
       <Head>
         <title>ユーザー管理</title>
       </Head>
 
-      <SettingMenu select={Select.User} title='ユーザー管理' bodyElement={<UserBody />} />
+      <SettingMenu
+        select={Select.User}
+        title='ユーザー管理'
+        isAdmin={props.isAdmin}
+        bodyElement={<UserBody />}
+      />
     </div>
   )
 }
 
-export default user
+export default User
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions)
+  if (!session) {
+    return {
+      props: {
+        isAdmin: false
+      }
+    }
+  }
+
+  const result = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id
+    },
+    select: {
+      is_admin: true
+    }
+  })
+
+  if (!result?.is_admin) {
+    return {
+      redirect: {
+        statusCode: 302,
+        destination: '/'
+      }
+    }
+  }
+
+  return {
+    props: {
+      isAdmin: result?.is_admin
+    }
+  }
+}
